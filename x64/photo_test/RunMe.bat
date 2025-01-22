2fdb6fd880b4e1db56ac78720c7a483086c0d413
prints-of-darkness <mccardlematthew0@gmail.com>
2025-01-22 10:28:14 -0500

    removed check
    I baked in the check stuff into the application. It was easier to do it that way.

--- x64/photo_test/RunMe.bat
+++ x64/photo_test/RunMe.bat
@@ -1,11 +1,6 @@
 @echo off
-rgb332.exe -h
-echo -----------------------------------------------
-rgb332.exe -i parrots.png -o parrots_dither.h -d
-echo -----------------------------------------------
-rgb332.exe -i parrots.png -o parrots.h
-echo -----------------------------------------------
-check.exe parrots_dither.h
-echo -----------------------------------------------
-check.exe parrots.h
+r3g3b2.exe -h
+echo ---------------------------------------------------------
+r3g3b2.exe -i parrots.png -o parrots_dither.h -d -debug
+echo ---------------------------------------------------------
 pause
