7ba057d8555506455d1c2a7c1ba992b26c7180ca
prints-of-darkness <mccardlematthew0@gmail.com>
2025-01-21 22:55:28 -0500

    now with iconic parrots.png
    change test again.

--- x64/photo_test/RunMe.bat
+++ x64/photo_test/RunMe.bat
@@ -1,11 +1,11 @@
 @echo off
 rgb332.exe -h
 echo -----------------------------------------------
-rgb332.exe -i ParrotRedMacaw.jpg -o ParrotRedMacaw_dither.h -d
+rgb332.exe -i parrots.png -o parrots_dither.h -d
 echo -----------------------------------------------
-rgb332.exe -i ParrotRedMacaw.jpg -o ParrotRedMacaw.h
+rgb332.exe -i parrots.png -o parrots.h
 echo -----------------------------------------------
-check.exe ParrotRedMacaw_dither.h
+check.exe parrots_dither.h
 echo -----------------------------------------------
-check.exe ParrotRedMacaw.h
+check.exe parrots.h
 pause
