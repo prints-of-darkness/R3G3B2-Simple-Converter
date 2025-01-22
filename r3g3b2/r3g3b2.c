05ba359b6b70395e5b5ef4033c705535f3b81019
prints-of-darkness <mccardlematthew0@gmail.com>
2025-01-22 17:27:53 -0500

    fixed gamma and contrast functions
    now applied to input, not output.
    also included lut gen at the bottom.

--- r3g3b2/r3g3b2.c
+++ r3g3b2/r3g3b2.c
@@ -9,6 +9,8 @@
            (outputs are almost the same, but not exactly the same)
 */
 
+
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -22,46 +24,46 @@
 
 #define MAX_FILENAME_LENGTH 1024
 
-/*
-    GAMMA 0.8
-    CONTRAST_FACTOR 1.0
-*/
+#define GAMMA 1.0
+#define CONTRAST_FACTOR 1.0
+
 static const uint32_t r3g3b2_to_r8g8b8_lut[256] = {
-    0x00000000, 0x0000004C, 0x000000A3, 0x000000FF, 0x00001D00, 0x00001D4C, 0x00001DA3, 0x00001DFF,
-    0x00004000, 0x0000404C, 0x000040A3, 0x000040FF, 0x00006400, 0x0000644C, 0x000064A3, 0x000064FF,
-    0x00008900, 0x0000894C, 0x000089A3, 0x000089FF, 0x0000B000, 0x0000B04C, 0x0000B0A3, 0x0000B0FF,
-    0x0000D700, 0x0000D74C, 0x0000D7A3, 0x0000D7FF, 0x0000FF00, 0x0000FF4C, 0x0000FFA3, 0x0000FFFF,
-    0x001D0000, 0x001D004C, 0x001D00A3, 0x001D00FF, 0x001D1D00, 0x001D1D4C, 0x001D1DA3, 0x001D1DFF,
-    0x001D4000, 0x001D404C, 0x001D40A3, 0x001D40FF, 0x001D6400, 0x001D644C, 0x001D64A3, 0x001D64FF,
-    0x001D8900, 0x001D894C, 0x001D89A3, 0x001D89FF, 0x001DB000, 0x001DB04C, 0x001DB0A3, 0x001DB0FF,
-    0x001DD700, 0x001DD74C, 0x001DD7A3, 0x001DD7FF, 0x001DFF00, 0x001DFF4C, 0x001DFFA3, 0x001DFFFF,
-    0x00400000, 0x0040004C, 0x004000A3, 0x004000FF, 0x00401D00, 0x00401D4C, 0x00401DA3, 0x00401DFF,
-    0x00404000, 0x0040404C, 0x004040A3, 0x004040FF, 0x00406400, 0x0040644C, 0x004064A3, 0x004064FF,
-    0x00408900, 0x0040894C, 0x004089A3, 0x004089FF, 0x0040B000, 0x0040B04C, 0x0040B0A3, 0x0040B0FF,
-    0x0040D700, 0x0040D74C, 0x0040D7A3, 0x0040D7FF, 0x0040FF00, 0x0040FF4C, 0x0040FFA3, 0x0040FFFF,
-    0x00640000, 0x0064004C, 0x006400A3, 0x006400FF, 0x00641D00, 0x00641D4C, 0x00641DA3, 0x00641DFF,
-    0x00644000, 0x0064404C, 0x006440A3, 0x006440FF, 0x00646400, 0x0064644C, 0x006464A3, 0x006464FF,
-    0x00648900, 0x0064894C, 0x006489A3, 0x006489FF, 0x0064B000, 0x0064B04C, 0x0064B0A3, 0x0064B0FF,
-    0x0064D700, 0x0064D74C, 0x0064D7A3, 0x0064D7FF, 0x0064FF00, 0x0064FF4C, 0x0064FFA3, 0x0064FFFF,
-    0x00890000, 0x0089004C, 0x008900A3, 0x008900FF, 0x00891D00, 0x00891D4C, 0x00891DA3, 0x00891DFF,
-    0x00894000, 0x0089404C, 0x008940A3, 0x008940FF, 0x00896400, 0x0089644C, 0x008964A3, 0x008964FF,
-    0x00898900, 0x0089894C, 0x008989A3, 0x008989FF, 0x0089B000, 0x0089B04C, 0x0089B0A3, 0x0089B0FF,
-    0x0089D700, 0x0089D74C, 0x0089D7A3, 0x0089D7FF, 0x0089FF00, 0x0089FF4C, 0x0089FFA3, 0x0089FFFF,
-    0x00B00000, 0x00B0004C, 0x00B000A3, 0x00B000FF, 0x00B01D00, 0x00B01D4C, 0x00B01DA3, 0x00B01DFF,
-    0x00B04000, 0x00B0404C, 0x00B040A3, 0x00B040FF, 0x00B06400, 0x00B0644C, 0x00B064A3, 0x00B064FF,
-    0x00B08900, 0x00B0894C, 0x00B089A3, 0x00B089FF, 0x00B0B000, 0x00B0B04C, 0x00B0B0A3, 0x00B0B0FF,
-    0x00B0D700, 0x00B0D74C, 0x00B0D7A3, 0x00B0D7FF, 0x00B0FF00, 0x00B0FF4C, 0x00B0FFA3, 0x00B0FFFF,
-    0x00D70000, 0x00D7004C, 0x00D700A3, 0x00D700FF, 0x00D71D00, 0x00D71D4C, 0x00D71DA3, 0x00D71DFF,
-    0x00D74000, 0x00D7404C, 0x00D740A3, 0x00D740FF, 0x00D76400, 0x00D7644C, 0x00D764A3, 0x00D764FF,
-    0x00D78900, 0x00D7894C, 0x00D789A3, 0x00D789FF, 0x00D7B000, 0x00D7B04C, 0x00D7B0A3, 0x00D7B0FF,
-    0x00D7D700, 0x00D7D74C, 0x00D7D7A3, 0x00D7D7FF, 0x00D7FF00, 0x00D7FF4C, 0x00D7FFA3, 0x00D7FFFF,
-    0x00FF0000, 0x00FF004C, 0x00FF00A3, 0x00FF00FF, 0x00FF1D00, 0x00FF1D4C, 0x00FF1DA3, 0x00FF1DFF,
-    0x00FF4000, 0x00FF404C, 0x00FF40A3, 0x00FF40FF, 0x00FF6400, 0x00FF644C, 0x00FF64A3, 0x00FF64FF,
-    0x00FF8900, 0x00FF894C, 0x00FF89A3, 0x00FF89FF, 0x00FFB000, 0x00FFB04C, 0x00FFB0A3, 0x00FFB0FF,
-    0x00FFD700, 0x00FFD74C, 0x00FFD7A3, 0x00FFD7FF, 0x00FFFF00, 0x00FFFF4C, 0x00FFFFA3, 0x00FFFFFF
+    0x00000000, 0x00000055, 0x000000AA, 0x000000FF, 0x00002400, 0x00002455, 0x000024AA, 0x000024FF,
+    0x00004800, 0x00004855, 0x000048AA, 0x000048FF, 0x00006D00, 0x00006D55, 0x00006DAA, 0x00006DFF,
+    0x00009100, 0x00009155, 0x000091AA, 0x000091FF, 0x0000B600, 0x0000B655, 0x0000B6AA, 0x0000B6FF,
+    0x0000DA00, 0x0000DA55, 0x0000DAAA, 0x0000DAFF, 0x0000FF00, 0x0000FF55, 0x0000FFAA, 0x0000FFFF,
+    0x00240000, 0x00240055, 0x002400AA, 0x002400FF, 0x00242400, 0x00242455, 0x002424AA, 0x002424FF,
+    0x00244800, 0x00244855, 0x002448AA, 0x002448FF, 0x00246D00, 0x00246D55, 0x00246DAA, 0x00246DFF,
+    0x00249100, 0x00249155, 0x002491AA, 0x002491FF, 0x0024B600, 0x0024B655, 0x0024B6AA, 0x0024B6FF,
+    0x0024DA00, 0x0024DA55, 0x0024DAAA, 0x0024DAFF, 0x0024FF00, 0x0024FF55, 0x0024FFAA, 0x0024FFFF,
+    0x00480000, 0x00480055, 0x004800AA, 0x004800FF, 0x00482400, 0x00482455, 0x004824AA, 0x004824FF,
+    0x00484800, 0x00484855, 0x004848AA, 0x004848FF, 0x00486D00, 0x00486D55, 0x00486DAA, 0x00486DFF,
+    0x00489100, 0x00489155, 0x004891AA, 0x004891FF, 0x0048B600, 0x0048B655, 0x0048B6AA, 0x0048B6FF,
+    0x0048DA00, 0x0048DA55, 0x0048DAAA, 0x0048DAFF, 0x0048FF00, 0x0048FF55, 0x0048FFAA, 0x0048FFFF,
+    0x006D0000, 0x006D0055, 0x006D00AA, 0x006D00FF, 0x006D2400, 0x006D2455, 0x006D24AA, 0x006D24FF,
+    0x006D4800, 0x006D4855, 0x006D48AA, 0x006D48FF, 0x006D6D00, 0x006D6D55, 0x006D6DAA, 0x006D6DFF,
+    0x006D9100, 0x006D9155, 0x006D91AA, 0x006D91FF, 0x006DB600, 0x006DB655, 0x006DB6AA, 0x006DB6FF,
+    0x006DDA00, 0x006DDA55, 0x006DDAAA, 0x006DDAFF, 0x006DFF00, 0x006DFF55, 0x006DFFAA, 0x006DFFFF,
+    0x00910000, 0x00910055, 0x009100AA, 0x009100FF, 0x00912400, 0x00912455, 0x009124AA, 0x009124FF,
+    0x00914800, 0x00914855, 0x009148AA, 0x009148FF, 0x00916D00, 0x00916D55, 0x00916DAA, 0x00916DFF,
+    0x00919100, 0x00919155, 0x009191AA, 0x009191FF, 0x0091B600, 0x0091B655, 0x0091B6AA, 0x0091B6FF,
+    0x0091DA00, 0x0091DA55, 0x0091DAAA, 0x0091DAFF, 0x0091FF00, 0x0091FF55, 0x0091FFAA, 0x0091FFFF,
+    0x00B60000, 0x00B60055, 0x00B600AA, 0x00B600FF, 0x00B62400, 0x00B62455, 0x00B624AA, 0x00B624FF,
+    0x00B64800, 0x00B64855, 0x00B648AA, 0x00B648FF, 0x00B66D00, 0x00B66D55, 0x00B66DAA, 0x00B66DFF,
+    0x00B69100, 0x00B69155, 0x00B691AA, 0x00B691FF, 0x00B6B600, 0x00B6B655, 0x00B6B6AA, 0x00B6B6FF,
+    0x00B6DA00, 0x00B6DA55, 0x00B6DAAA, 0x00B6DAFF, 0x00B6FF00, 0x00B6FF55, 0x00B6FFAA, 0x00B6FFFF,
+    0x00DA0000, 0x00DA0055, 0x00DA00AA, 0x00DA00FF, 0x00DA2400, 0x00DA2455, 0x00DA24AA, 0x00DA24FF,
+    0x00DA4800, 0x00DA4855, 0x00DA48AA, 0x00DA48FF, 0x00DA6D00, 0x00DA6D55, 0x00DA6DAA, 0x00DA6DFF,
+    0x00DA9100, 0x00DA9155, 0x00DA91AA, 0x00DA91FF, 0x00DAB600, 0x00DAB655, 0x00DAB6AA, 0x00DAB6FF,
+    0x00DADA00, 0x00DADA55, 0x00DADAAA, 0x00DADAFF, 0x00DAFF00, 0x00DAFF55, 0x00DAFFAA, 0x00DAFFFF,
+    0x00FF0000, 0x00FF0055, 0x00FF00AA, 0x00FF00FF, 0x00FF2400, 0x00FF2455, 0x00FF24AA, 0x00FF24FF,
+    0x00FF4800, 0x00FF4855, 0x00FF48AA, 0x00FF48FF, 0x00FF6D00, 0x00FF6D55, 0x00FF6DAA, 0x00FF6DFF,
+    0x00FF9100, 0x00FF9155, 0x00FF91AA, 0x00FF91FF, 0x00FFB600, 0x00FFB655, 0x00FFB6AA, 0x00FFB6FF,
+    0x00FFDA00, 0x00FFDA55, 0x00FFDAAA, 0x00FFDAFF, 0x00FFFF00, 0x00FFFF55, 0x00FFFFAA, 0x00FFFFFF
 };
 
-unsigned char r8g8b8_to_r3g3b2(uint8_t red, uint8_t green, uint8_t blue);
+//unsigned char r8g8b8_to_r3g3b2(uint8_t red, uint8_t green, uint8_t blue);
+unsigned char r8g8b8_to_r3g3b2(uint8_t red, uint8_t green, uint8_t blue, float gamma, float contrast);
 void floydSteinbergDither(uint8_t* data, int width, int height);
 
 int main(int argc, char* argv[])
@@ -156,7 +158,7 @@ int main(int argc, char* argv[])
             for (_y = 0; _y < y; _y++) {
                 for (_x = 0; _x < x; _x++) {
                     pixel = data + (_y * x + _x) * 3;
-                    pixel332 = r8g8b8_to_r3g3b2(pixel[0], pixel[1], pixel[2]);
+                    pixel332 = r8g8b8_to_r3g3b2(pixel[0], pixel[1], pixel[2], GAMMA, CONTRAST_FACTOR);
                     fprintf(fp, "0x%.2X, ", pixel332);
                     if (debug) {
                         pixel[0] = (r3g3b2_to_r8g8b8_lut[pixel332] >> 16) & 0xFF;
@@ -195,9 +197,36 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-unsigned char r8g8b8_to_r3g3b2(uint8_t red, uint8_t green, uint8_t blue) {
-    return ((red & 0xE0) | ((green & 0xE0) >> 3) | (blue >> 6));
+//unsigned char r8g8b8_to_r3g3b2(uint8_t red, uint8_t green, uint8_t blue) {
+//    return ((red & 0xE0) | ((green & 0xE0) >> 3) | (blue >> 6));
+//}
+
+unsigned char r8g8b8_to_r3g3b2(uint8_t red, uint8_t green, uint8_t blue, float gamma, float contrast) {
+    // Apply gamma correction
+    float r = pow(red / 255.0f, gamma) * 255.0f;
+    float g = pow(green / 255.0f, gamma) * 255.0f;
+    float b = pow(blue / 255.0f, gamma) * 255.0f;
+
+    // Apply contrast adjustment
+    float factor = (259.0f * (contrast + 255.0f)) / (255.0f * (259.0f - contrast));
+    r = factor * (r - 128.0f) + 128.0f;
+    g = factor * (g - 128.0f) + 128.0f;
+    b = factor * (b - 128.0f) + 128.0f;
+
+    // Clamp values to 0-255 range
+    r = fmaxf(0.0f, fminf(r, 255.0f));
+    g = fmaxf(0.0f, fminf(g, 255.0f));
+    b = fmaxf(0.0f, fminf(b, 255.0f));
+
+    // Convert back to uint8_t
+    uint8_t r_adj = (uint8_t)r;
+    uint8_t g_adj = (uint8_t)g;
+    uint8_t b_adj = (uint8_t)b;
+
+    // Perform the original conversion
+    return ((r_adj & 0xE0) | ((g_adj & 0xE0) >> 3) | (b_adj >> 6));
 }
+
 
 void floydSteinbergDither(uint8_t* data, int width, int height) {
     for (int y = 0; y < height; y++) {
@@ -245,3 +274,44 @@ void floydSteinbergDither(uint8_t* data, int width, int height) {
     }
 }
 
+//#include <stdio.h>
+//#include <stdint.h>
+//#include <math.h>
+//
+//#define GAMMA 1.0
+//#define CONTRAST_FACTOR 1.0
+//
+//uint32_t generate_corrected_color(uint8_t value, uint8_t max_value) {
+//    double normalized = (double)value / max_value;
+//    double corrected = pow(normalized, GAMMA) * 255 * CONTRAST_FACTOR;
+//    return (uint32_t)(fmin(fmax(corrected, 0), 255));
+//}
+//
+//void generate_brightness_corrected_lut(uint32_t lut[256]) {
+//    for (int i = 0; i < 256; i++) {
+//        uint8_t r = (i >> 5) & 0x07;
+//        uint8_t g = (i >> 2) & 0x07;
+//        uint8_t b = i & 0x03;
+//
+//        uint32_t r8 = generate_corrected_color(r, 7);
+//        uint32_t g8 = generate_corrected_color(g, 7);
+//        uint32_t b8 = generate_corrected_color(b, 3);
+//
+//        lut[i] = (r8 << 16) | (g8 << 8) | b8;
+//    }
+//}
+//
+//int main() {
+//    uint32_t corrected_lut[256];
+//    generate_brightness_corrected_lut(corrected_lut);
+//
+//    printf("static const uint32_t r3g3b2_to_r8g8b8_lut[256] = {\n");
+//    for (int i = 0; i < 256; i++) {
+//        printf("0x%08X", corrected_lut[i]);
+//        if (i < 255) printf(", ");
+//        if ((i + 1) % 8 == 0) printf("\n");
+//    }
+//    printf("};\n");
+//
+//    return 0;
+//}
