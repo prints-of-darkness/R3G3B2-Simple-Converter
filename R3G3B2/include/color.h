b5b2515361e96bef2492e4184915f8b9252996e6
prints-of-darkness <mccardlematthew0@gmail.com>
2025-01-28 20:45:13 -0500

    Update color.h
    missed deleted function declaration, now removed

--- R3G3B2/include/color.h
+++ R3G3B2/include/color.h
@@ -20,7 +20,6 @@ typedef struct {
 } RGBColor;
 
 uint8_t reduceBits(uint8_t value, int max_value);
-float colorDistance(const RGBColor* color1, const RGBColor* color2);
 uint8_t mapColorToR3G3B2_Reduced(uint8_t r, uint8_t g, uint8_t b);
 void quantize_pixel_with_map_reduced(uint8_t* r, uint8_t* g, uint8_t* b);
 uint8_t rgbToRgb332(uint8_t r, uint8_t g, uint8_t b);
